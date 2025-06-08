988769bf1b0313f083f64cdf9b087e1e13af12e1
Rangga Alroy <rangga.alroy@gmail.com>
2025-06-08 20:41:35 +0700

    fix: eslint prettier and tailwind setting

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import './globals.css';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-bold text-dark-200">Hello world</Text>
+    </View>
+  );
+}
